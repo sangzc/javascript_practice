227bba2a693594a62d5c03bd8105ea64b8461ef6
Sandy <sangzicao@gmail.com>
2019-08-14 00:41:57 -0700

    bubble sort, naive string, kmp

--- practice.js
+++ practice.js
@@ -54,3 +54,43 @@ var isValid = function(s) {
     }
     return opens.length === 0
 };
+
+// Search Naive String
+// count the number of times a string shows up in a larger string
+// outer loop and inner loop, inner loop breaks whenever outerloop + innerloop's index is not equal
+function naiveString(A, B) {
+    let count = 0;
+    for (let i=0; i<A.length; i++) {
+        for (let j=0; j<B.length; j++) {
+          if (B[j] !== A[i+j]) break
+          if (j === B.length-1) count++
+        }
+    }
+    return count || -1
+};
+
+// KMP Pattern Matching Substring Search - First Occurence Of SubString
+var strStr = function(haystack, needle) {
+    if (needle.length === 0) return 0
+    let result = -1
+    for (let i=0; i<haystack.length; i++) {
+        for (let j=0; j<needle.length; j++) {
+            if (haystack[i+j] !== needle[j]) break
+            if (j === needle.length-1) return i
+        }
+    }
+    return result;
+};
+
+// Bubble Sort
+function(nums) {
+    for (let i=nums.length-1; i>=0; i--) {
+        for (let j=0; j<i; j++) {
+            console.log(i, j)
+            if (nums[j] > nums[j+1]) {
+                [nums[j], nums[j+1]] = [nums[j+1], nums[j]]
+            }
+        }
+    }
+    return nums
+};
